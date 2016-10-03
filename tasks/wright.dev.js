65e9fc001299950be9538a6655cfe655cd3e48ff
Rasmus Porsager <rasmus@porsager.com>
2016-10-03 22:40:06 +0200

    Fix to work with injection

--- tasks/wright.dev.js
+++ tasks/wright.dev.js
@@ -1,28 +1,23 @@
 const fs = require('fs-extra')
-    , chokidar = require('chokidar')
     , rollup = require('rollup')
     , commonjs = require('rollup-plugin-commonjs')
     , json = require('rollup-plugin-json')
-    , buble = require('rollup-plugin-buble')
-    , uglify = require('rollup-plugin-uglify')    
     , nodeResolve = require('rollup-plugin-node-resolve')
     , postcss = require('postcss')
     , wright = require('wright')
 
-
-fs.removeSync('workspace/build');
+fs.removeSync('workspace/build')
 
 wright({
-  main: 'http://wright-test.dev',
   debug: true,
   fps: true,
   run: 'm.redraw()',
   js: {
-    watch: '/workspace/source/js/**/*.js',
+    watch: 'workspace/source/js/**/*.js',
     compile: roll
   },
   css: {
-    watch: '/workspace/source/css/**/*.css',
+    watch: 'workspace/source/css/**/*.css',
     compile: style
   },
   watch: ['**/*.html']
@@ -34,61 +29,37 @@ function roll() {
     plugins: [
       json(),
       commonjs(),
-      nodeResolve(),
-      buble(),
-      uglify({ mangle: true, compress: true })
+      nodeResolve()
     ]
+  }).then(bundle => {
+    return bundle.generate({
+      format: 'iife',
+      moduleName: 'tipografics'
+    }).code
   })
- .then(bundle => {
-  bundle.write({
-    format: 'iife',
-    moduleName: 'tipografics',    
-    dest: 'workspace/build/js/app.js',
-    sourceMap: true
-  })
-}).catch(e => {
-  console.error(e)
-  // process.exit(1)
-})   
 }
 
 function style() {
-  return new Promise((resolve, reject) => {
-    fs.readFile('workspace/source/css/app.css', 'utf8', (err, str) => {
-      if (err)
-        return reject(err)
-
-        postcss([ 
-          require('postcss-import'),          
-          require('autoprefixer')({ browsers: ['last 3 versions', '> 5%', 'ie >= 10'] }),
-          require('postcss-custom-properties'),
-          require('postcss-mixins'),
-          require('postcss-simple-vars'),
-          require('postcss-color-function'),
-          require('postcss-nested'),
-          require('postcss-typescale'),
-          require('postcss-atroot'),
-		      require('postcss-button'),          
-          require('cssnano')(
-            {
-              discardComments: {
-                removeAll: false
-              }
-            }
-          ),
-          require('precss')             
-        ])
-        .process(str, { 
-          from: 'workspace/source/css/app.css', 
-          to: 'workspace/build/css/app.css',
-          map: { inline: false } 
-        })
-        .then(function (css) {
-            fs.outputFile('workspace/build/css/app.css', css.css);
-            if ( css.map ) fs.outputFile('workspace/build/css/app.css.map', css.map);
-        }).catch(error => {
-  			console.error(error);
-		});
-    })
-  })
+  return postcss([
+    require('postcss-import'),
+    require('autoprefixer')({ browsers: ['last 3 versions', '> 5%', 'ie >= 10'] }),
+    require('postcss-custom-properties'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-color-function'),
+    require('postcss-nested'),
+    require('postcss-typescale'),
+    require('postcss-atroot'),
+    require('postcss-button'),
+    require('cssnano')(
+      {
+        discardComments: {
+          removeAll: false
+        }
+      }
+    ),
+    require('precss')
+  ])
+  .process(fs.readFileSync('workspace/source/css/app.css', 'utf8'))
+  .then(result => result.css)
 }
